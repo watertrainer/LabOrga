26f518209db33c603f1334ac7910ce603bf73649
watertrainer <watertrainer42@gmail.com>
2019-08-21 23:03:17 +0200

    Merge branch 'master' into aSubjectsHachMap
    # Conflicts:
    #	.idea/LabOrga.iml
    Transfered aSubjects to HashMap

--- src/GUI.java
+++ src/GUI.java
@@ -11,7 +11,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.*;
-
 public class GUI extends JFrame {
     public JTabbedPane tabbedPane1;
     public JPanel panel1;
